34f073647c9019f250fd805075cecb4a0277534e
Murilo Rocha <muriloamaralrocha@outlook.com>
2025-04-16 23:17:13 -0300

    update

--- nky/nky.py
+++ nky/nky.py
@@ -1,4 +1,5 @@
 import discord
+import re
 from redbot.core import commands
 
 class nky(commands.Cog):
@@ -14,9 +15,13 @@ class nky(commands.Cog):
         if message.author == self.bot.user:
             return
         
-        # Check if the message contains the word "hello"
-        if "kill myself" in message.content.lower() or "kms" in message.content.lower():
-            await message.channel.send(f"https://cdn.discordapp.com/attachments/241369543572848640/1362212240736125050/Never_Kill_yourself.mp4?ex=68019282&is=68004102&hm=85c26be41c922d0364c69b653f67cab2e0d739fc25556c796c156f912f70908d&")
+        # List of greetings to look for
+        greetings = ["kms", "kill myself"]
+
+        # Check if any greeting is in the message
+        for greeting in greetings:
+            if re.search(rf'\b{greeting}\b', message.content.lower()):
+                await message.channel.send(f"https://cdn.discordapp.com/attachments/241369543572848640/1362212240736125050/Never_Kill_yourself.mp4?ex=68019282&is=68004102&hm=85c26be41c922d0364c69b653f67cab2e0d739fc25556c796c156f912f70908d&")
 
 
 def setup(bot):
