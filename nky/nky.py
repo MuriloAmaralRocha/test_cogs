b948eec891a16d2692030f869c4f68099243297f
Murilo Rocha <muriloamaralrocha@outlook.com>
2025-05-12 19:09:37 -0300

    refactor: change variable names

--- nky/nky.py
+++ nky/nky.py
@@ -16,11 +16,11 @@ class nky(commands.Cog):
             return
         
         # List of greetings to look for
-        greetings = ["kms", "kill myself", "me matar", "me mata"]
+        words = ["kms", "kill myself", "me matar", "me mata"]
 
         # Check if any greeting is in the message
-        for greeting in greetings:
-            if re.search(rf'\b{greeting}\b', message.content.lower()):
+        for word in words:
+            if re.search(rf'\b{word}\b', message.content.lower()):
                 await message.channel.send(f"https://cdn.discordapp.com/attachments/241369543572848640/1362212240736125050/Never_Kill_yourself.mp4?ex=68019282&is=68004102&hm=85c26be41c922d0364c69b653f67cab2e0d739fc25556c796c156f912f70908d&")
                 return
 
