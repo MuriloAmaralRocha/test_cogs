ca3f3fb979f37b41a69aa88a029a3ba15004c8df
Murilo Rocha <muriloamaralrocha@outlook.com>
2025-05-12 19:08:34 -0300

    fix: fix trigering multiple times on the same message

--- nky/nky.py
+++ nky/nky.py
@@ -22,6 +22,7 @@ class nky(commands.Cog):
         for greeting in greetings:
             if re.search(rf'\b{greeting}\b', message.content.lower()):
                 await message.channel.send(f"https://cdn.discordapp.com/attachments/241369543572848640/1362212240736125050/Never_Kill_yourself.mp4?ex=68019282&is=68004102&hm=85c26be41c922d0364c69b653f67cab2e0d739fc25556c796c156f912f70908d&")
+                return
 
 
 def setup(bot):
