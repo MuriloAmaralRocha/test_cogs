ccd633485c26aface96100ae5886c4ad76907d89
Murilo Rocha <muriloamaralrocha@outlook.com>
2025-09-03 11:06:27 -0300

    fix: fix bugs

--- nky/nky.py
+++ nky/nky.py
@@ -18,23 +18,24 @@ class nky(commands.Cog):
         
         # List of greetings to look for
         words = ["kms", "kill myself", "me matar", "me mata"]
+        if not any(re.search(rf'\b{word}\b', message.content.lower()) for word in words):
+            return
 
         wordToSearch = "never kill yourself"
         user = "Showori_"
         query = f'from:{user} "{wordToSearch}"'
 
-        # Check if any greeting is in the message
-        for word in words:
-            if re.search(rf'\b{word}\b', message.content.lower()):
-                tweet = None
-                for t in sntwitter.TwitterSearchScraper(query).get_items():
-                    tweet = t
-                    break
-                if tweet:
-                    await message.channel.send(f"{tweet.url}")
-                    return
-                await message.channel.send(f"https://cdn.discordapp.com/attachments/241369543572848640/1362212240736125050/Never_Kill_yourself.mp4?ex=68019282&is=68004102&hm=85c26be41c922d0364c69b653f67cab2e0d739fc25556c796c156f912f70908d&")
-                return
+        tweet = None
+        for t in sntwitter.TwitterSearchScraper(query).get_items():
+            tweet = t
+            break
+
+        if tweet:
+            await message.channel.send(tweet.url)
+        else:
+            await message.channel.send(
+                "https://cdn.discordapp.com/attachments/241369543572848640/1362212240736125050/Never_Kill_yourself.mp4"
+            )
 
 
 def setup(bot):
