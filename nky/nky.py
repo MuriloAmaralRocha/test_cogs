a187cf3921e2a6edc14ccb72328057a74c72d6da
Murilo Rocha <muriloamaralrocha@outlook.com>
2025-09-03 11:12:20 -0300

    test

--- nky/nky.py
+++ nky/nky.py
@@ -24,6 +24,5 @@ class nky(commands.Cog):
                 await message.channel.send(f"https://cdn.discordapp.com/attachments/241369543572848640/1362212240736125050/Never_Kill_yourself.mp4?ex=68019282&is=68004102&hm=85c26be41c922d0364c69b653f67cab2e0d739fc25556c796c156f912f70908d&")
                 return
 
-
 def setup(bot):
     bot.add_cog(nky(bot))
